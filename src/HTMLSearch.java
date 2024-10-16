dc9ceabee666b9b967ea361f60f11e7f3c4d778d
blegrand25 <breah_legrand25@milton.edu>
2024-10-16 08:33:02 -0400

    Initial commit

---
+++ src/HTMLSearch.java
@@ -0,0 +1,22 @@
+public class HTMLSearch {
+    public static void main(String[] args) {
+        HTMLSearch Search = new HTMLSearch();
+    }
+
+    // your task!
+    //***/** make text field where user can enter a URL
+    /**TO DO!
+     * make text field where user can enter a URL
+     *have a text field where the user can enter a serach term
+     * search throughout the url's html code for all webpages line
+     * display the found links in a non-editable text area!
+     * */
+
+    public HTMLSearch(){
+
+
+
+    }
+
+    public void field (){}
+}
